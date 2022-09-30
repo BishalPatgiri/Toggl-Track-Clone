d08bb139ec1c6d19a049943374034da811345ea9
Kumar Gaurav <99576535+Kgaurav113@users.noreply.github.com>
2022-09-30 08:08:47 +0530

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 <<<<<<< HEAD
 import { ChakraProvider } from '@chakra-ui/react';
 import {BrowserRouter} from "react-router-dom"
@@ -14,17 +14,5 @@ root.render(
     <App />
     </BrowserRouter>
     </ChakraProvider>
-=======
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> 1ef4e412e1905cf682a4320136008727f492a6f6
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
