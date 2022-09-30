2d784b04de1d68a42020d59077790f81190df07b
Indherjith <buvanesifet26@gmail.com>
2022-09-30 13:49:00 +0530

    main

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import Login from "./Components/Signing/Login";
 import Price from "./prices/Prices";
-
+import Client from "./Clients/Client";
 
 function App() {
   return (
     <div className="App">
      {/* <Login/> */}
-     <Price/>
+     {/* <Price/> */}
+     <Client/>
     </div>
   );
 }
