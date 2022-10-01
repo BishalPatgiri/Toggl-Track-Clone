e84a07ae3bebd86130ca4efd03bd4f19c3a32b86
Kumar Gaurav <kgaurav501@gmail.com>
2022-10-01 09:15:19 +0530

    final

--- Backend/backend/models/clientmodel.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose");
-const ClientSchema= new mongoose.Schema({
-   
-    clientname:{type:String,required:true},
-    userId:{type:String,required:true}
-})
-const ClientModel= mongoose.model("Clients",ClientSchema)
-module.exports=ClientModel;
